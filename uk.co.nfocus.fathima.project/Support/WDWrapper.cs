b551bba55a14ee201f3b15adf5fc7e5f3c247b62
Fathima Jamal-Deen <fathima.jamal-deen@nfocus.co.uk>
2024-03-14 12:57:16 +0000

    Adding a wrapper class for the driver to make it type safe

---
+++ uk.co.nfocus.fathima.project/Support/WDWrapper.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace uk.co.nfocus.fathima.project.Support
+{
+    public class WDWrapper
+    {
+        private IWebDriver _driver;
+
+        public IWebDriver Driver { get => _driver; set => _driver = value; }
+    }
+}
